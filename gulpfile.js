11d296bfc35e9a2f0547fe4eb16c77282091d6df
Samurai6111 <samurai.blue6111@gmail.com>
2023-03-13 14:11:36 +0900

    仮完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,14 @@
-// 1. module読み込み
-const { src, dest, watch, series, parallel } = require("gulp");
+let gulp = require("gulp");
+let sass = require("gulp-sass")(require("sass"));
+let scss_dir = './assets/Scss/*.scss'
+let css_dir = './assets/Css'
 
-//sass
-const plumber = require("gulp-plumber");
-const sass = require("gulp-sass")(require("sass"));
-const notify = require("gulp-notify");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("gulp-autoprefixer");
-const cleanCSS = require("gulp-clean-css");
-const rename = require("gulp-rename");
-const sourcemaps = require("gulp-sourcemaps");
-const mqpacker = require("css-mqpacker");
+gulp.task("sass", function(){
+    gulp.src(scss_dir)
+    .pipe(sass({outputStyle: "compressed"}))
+    .pipe(gulp.dest(css_dir))
+});
 
-//ブラウザリロード
-const browserSync = require("browser-sync");
-
-//参照元パス
-const srcPath = {
-    css: "asset/scss/*.scss",
-};
-
-//出力先パス
-const destPath = {
-    css: "asset/css/",
-};
-
-// 2. 関数定義
-//sassコンパイル
-const cssSass = () => {
-    return src(srcPath.css) //コンパイル元
-        .pipe(
-            plumber(
-                //エラーが出ても処理を止めない
-                {
-                    errorHandler: notify.onError("Error:<%= error.message %>"),
-                    //エラー出力設定
-                }
-            )
-        )
-        .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: "expanded" }))
-        .pipe(postcss([mqpacker()])) // メディアクエリを圧縮
-        .pipe(autoprefixer()) // ベンダープレフィックスを付与
-        .pipe(sourcemaps.write())
-        .pipe(dest(destPath.css)) //コンパイル先（style.css）
-        .pipe(cleanCSS()) // CSS圧縮
-        .pipe(
-            rename({
-                extname: ".min.css", //.min.cssの拡張子にする
-            })
-        )
-        .pipe(dest(destPath.min_css)); //コンパイル先（style.min.css）
-};
-
-//ローカルサーバー立ち上げ、ファイル監視と自動リロード
-const browserSyncFunc = () => {
-    browserSync.init(browserSyncOption);
-};
-const browserSyncOption = {
-    proxy: "enowa.local", //環境によって変更する
-    open: true,
-    reloadOnRestart: true,
-    notify: false,
-};
-
-//リロード
-const browserSyncReload = (done) => {
-    browserSync.reload();
-    done();
-};
-
-// 3. タスク定義
-const watchFiles = () => {
-    watch(srcPath.css, series(cssSass, browserSyncReload));
-};
-exports.default = series(series(cssSass, js_min), parallel(watchFiles, browserSyncFunc));
+gulp.task("watch", function(){
+    gulp.watch(scss_dir, gulp.series(["sass"]))
+});
